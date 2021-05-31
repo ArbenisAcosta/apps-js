09049b9e0aef1e50258148b91ca11da99ba0c235
Arbenis Andres Acosta <45821765+ArbenisAcosta@users.noreply.github.com>
2021-05-31 14:24:21 -0400

    add TodoList-Js
    Agregando una pequeña practica de todoList.

---
+++ JSTodoList-start/js/model.js
@@ -0,0 +1,66 @@
+export default class Model {
+  constructor() {
+    this.view = null;
+    this.todos = JSON.parse(localStorage.getItem('todos'));
+    if (!this.todos || this.todos.length < 1) {
+      this.todos = [
+        {
+          id: 0,
+          title: 'Learn Js',
+          description: 'Watch JS tutorials',
+          completed: false,
+        },
+      ];
+      this.currentId = 1;
+    } else {
+      this.currentId = this.todos[this.todos.length - 1].id + 1;
+    }
+  }
+
+  save() {
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
+
+  setView(view) {
+    this.view = view;
+  }
+
+  getTodos() {
+    return this.todos.map((todo) => ({ ...todo }));
+  }
+
+  findTodo(id) {
+    return this.todos.findIndex((todo) => todo.id === id);
+  }
+
+  toggleCompleted(id) {
+    const index = this.findTodo(id);
+    const todo = this.todos[index];
+    todo.completed = !todo.completed;
+    this.save();
+  }
+
+  editTodo(id, values) {
+    const index = this.findTodo(id);
+    Object.assign(this.todos[index], values);
+    this.save();
+  }
+  addTodo(title, description) {
+    const todo = {
+      id: this.currentId++,
+      title,
+      description,
+      completed: false,
+    };
+    this.todos.push(todo);
+    this.save();
+
+    return { ...todo };
+  }
+
+  removeTodo(id) {
+    const index = this.findTodo(id);
+    this.todos.splice(index, 1);
+    this.save();
+  }
+}
